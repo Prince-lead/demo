b0d96b2639889d0577ec84b54d51c5bfc4ff8557
Prince_1973 <Princemaini22@gmail.com>
2023-08-05 15:09:52 +0530

    Added description for what thing my city is famous for and fixed previous issue after print statemet there is showing s extra

--- src/GIT.java
+++ src/GIT.java
@@ -3,7 +3,8 @@ public class GIT {
 
 	public static void main(String[] args) {
 		System.out.println("My name is prince maini");
-System.out.println("I am from fazilka punjab");s
+System.out.println("I am from fazilka punjab");
+System.out.println("My city is famous for tosha sweet and from our city they export tosha all over the india even they are selling the tosha on amazon also");
 
 	}
 
