c882b2c06b94b1504aa785e9f751fe2d969f1271
Prince_1973 <Princemaini22@gmail.com>
2023-08-05 15:32:37 +0530

    Added one description for fazilka city

--- src/GIT.java
+++ src/GIT.java
@@ -5,6 +5,7 @@ public class GIT {
 		System.out.println("My name is prince maini");
 System.out.println("I am from fazilka punjab");
 System.out.println("My city is famous for tosha sweet and from our city they export tosha all over the india even they are selling the tosha on amazon also");
+System.out.println("My city is also famous for the tv tower it is second largest tower in punjab after burj khalifa")
 
 	}
 
